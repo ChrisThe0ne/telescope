5d99f069b4a28e81bbc53727db22711b9bd1f8b2
ChrisThe0ne <bencsikkrisztian1999@gmail.com>
2025-02-19 05:55:26 +0100

    TELE-3 Set Up Spring Boot Backend
    Initialize a new Spring Boot project with the necessary dependencies:
    -Spring Web
    -Spring Security
    -Spring Data JPA
    -OpenTelemetry SDK

---
+++ src/main/java/dev/bencsik/telescope/TeleScopeApplication.java
@@ -0,0 +1,11 @@
+package dev.bencsik.telescope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleScopeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TeleScopeApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/bencsik/telescope/TelescopeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bencsik.telescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelescopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
